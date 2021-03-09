c1fa9665a0aadda9f2ace1b5c0d2011a32a01471
Johan Griesel <jdgriesel@gmail.com>
2021-03-09 14:13:29 +0200

    Update data-layer.js

--- dx-core-modules/data-layer.js
+++ dx-core-modules/data-layer.js
@@ -2,15 +2,15 @@ class DivbloxDataLayer {
     constructor(database_connector = null,data_model = {}) {
         this.error_info = [];
         this.data_model = data_model;
+        this.data_model_entities = Object.keys(this.data_model);
         this.required_entities = ["Account"];
         if (!this.validateDataModel()) {
             throw new Error("Error validating data model: "+JSON.stringify(this.error_info));
         }
     }
     validateDataModel() {
-        const data_model_entities = Object.keys(this.data_model);
         for (const entity of this.required_entities) {
-            if (data_model_entities.indexOf(entity) === -1) {
+            if (this.data_model_entities.indexOf(entity) === -1) {
                 this.error_info.push("Entity '"+entity+"' not present");
             }
         }
@@ -19,6 +19,18 @@ class DivbloxDataLayer {
             return false;
         }
         return true;
+    }
+    create(entity_name = '',data = {}) {
+        this.error_info = [];
+        if (!this.checkEntityExistsInDataModel(entity_name)) {
+            this.error_info.push("Entity "+entity_name+" does not exist");
+            return false;
+        }
+        
+        const query_str = "INSERT INTO `"+entity_name+"`"
+    }
+    checkEntityExistsInDataModel(entity_name = '') {
+        return this.data_model_entities.indexOf(entity_name.toLowerCase()) !== -1;
     }
 }
 module.exports = DivbloxDataLayer;
