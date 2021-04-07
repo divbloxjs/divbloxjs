a14bd1e00e0a0a3ad0d1547bccf242d67c41d46f
Johan Griesel <jdgriesel@gmail.com>
2021-04-07 12:30:45 +0200

    Update init_dx.js

--- init_dx.js
+++ init_dx.js
@@ -3,9 +3,6 @@ const fs_async = require("fs").promises;
 const divblox_root = "../../divblox_config";
 const data_model_file_name = divblox_root+'/data-model.json';
 const dx_config_file_name = divblox_root+'/dxconfig.json';
-if (!fs.existsSync(divblox_root)){
-    fs.mkdirSync(divblox_root);
-}
 const dx_config_default = {
     "environment_array":{
         "development":{
@@ -29,15 +26,21 @@ const dx_config_default = {
             }
         }}
 };
-
 const dx_data_model_default = {};
 
 createDefaults();
 async function createDefaults() {
+    console.log("Initializing Divblox...");
+    if (!fs.existsSync(divblox_root)){
+        console.log("Creating Divblox config directory...");
+        fs.mkdirSync(divblox_root);
+    }
     if (!fs.existsSync(data_model_file_name)) {
+        console.log("Creating Divblox data model...");
         await fs_async.writeFile(data_model_file_name, JSON.stringify(dx_data_model_default,null,2));
     }
     if (!fs.existsSync(dx_config_file_name)) {
+        console.log("Creating Divblox default config file...");
         await fs_async.writeFile(dx_config_file_name, JSON.stringify(dx_config_default,null,2));
     }
 }
