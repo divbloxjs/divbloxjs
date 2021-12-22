ef9a638f776797313fd614fb0fc744341a164387
Johan Griesel <johan@stratusolve.com>
2021-12-22 09:35:38 +0200

    Update package-controller-base.js

--- dx-core-modules/package-controller-base.js
+++ dx-core-modules/package-controller-base.js
@@ -9,14 +9,18 @@ class DivbloxPackageControllerBase extends divbloxObjectBase {
     /**
      * A basic constructor that can be overridden
      * @param {DivbloxBase} dxInstance An instance of divbloxjs to allow for access to the app configuration
+     * @param {string} packageName The name given to this package
      */
-    constructor(dxInstance = null) {
+    constructor(dxInstance = null, packageName = 'base') {
         super();
         this.dxInstance = dxInstance;
+        this.packageName = packageName;
 
         if ((typeof this.dxInstance === "undefined") || (this.dxInstance === null)) {
             throw new Error("Divblox instance was not provided");
         }
+
+        this.packageOptions = this.dxInstance.getPackageOptions(this.packageName);
     }
 }
 
