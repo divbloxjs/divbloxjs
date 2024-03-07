2cc10b198fe8f355010dc64cc44d5b6efd545881
Dani <dani.simeonov@stratusolve.com>
2024-03-07 09:58:11 +0200

    Fix for dates being inserted into the database as 1970-01-01 for null values

--- dx-orm/object-model-base.js
+++ dx-orm/object-model-base.js
@@ -374,17 +374,17 @@ class DivbloxObjectModelBase extends DivbloxObjectBase {
             }
 
             if (["date", "date-time"].includes(this.entitySchema[attributeName]?.["format"])) {
-                inputDataAttributeValue = new Date(inputDataAttributeValue).getTime();
+                if (inputDataAttributeValue) {
+                    inputDataAttributeValue = new Date(inputDataAttributeValue);
+                    inputDataAttributeValue = inputDataAttributeValue.getTime();
+                }
+
                 lastLoadedDataAttributeValue =
                     lastLoadedDataAttributeValue !== null ? lastLoadedDataAttributeValue.getTime() : null;
             }
 
             if (inputDataAttributeValue === lastLoadedDataAttributeValue) {
                 continue;
-            }
-
-            if (["date", "date-time"].includes(this.entitySchema[attributeName]?.["format"])) {
-                inputDataAttributeValue = new Date(inputDataAttributeValue);
             }
 
             if (this.entitySchema[attributeName].hasOwnProperty("enum")) {
